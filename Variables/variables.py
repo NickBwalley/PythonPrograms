b7684e8d76d39bed92b17731580b3ac78a2cf60e
NickBwalley <nickbwalley@gmail.com>
2020-05-22 03:30:23 +0300

    python

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,5 +1,7 @@
 
-print("There was once a man named George")
-print("He was 70 Years old")
-print("He really loved his name George")
-print("But he didn't love his age of 70Years")
+character_name = "NickFury"
+character_age = "19"
+print("There was once a man named "+character_name + ",")
+print("He was " + character_age + " Years old")
+print("He really loved his name "+character_name)
+print("But he didn't love his age being "+character_age+"Years")
