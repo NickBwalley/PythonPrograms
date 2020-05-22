3ed3b24695bc2751028b3c578a8ce95c285f6dd4
NickBwalley <nickbwalley@gmail.com>
2020-05-22 03:54:07 +0300

    python

--- Strings/WorkingWithStrings.py
+++ Strings/WorkingWithStrings.py
@@ -1,4 +1,5 @@
 print("My name is NickBwalley\nI am a Coder") #\n the escape sequence used to print to a new line
 print("Also Called the \"Bronze_Bomber\"") #Prints a quotation mark on bronze Bomber word
-phrase = "PyCharm"
+phrase = "PYCHARM"
 print(phrase + " Is a cool IDE") #basically concatenations
+print(phrase.lower()) #converts the phrase variable to lowerCase
