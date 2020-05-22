a10f0783b01cb0c890c269d7a59f157b8708d667
NickBwalley <nickbwalley@gmail.com>
2020-05-22 04:35:35 +0300

    python

--- Numbers/WorkingWithNumbers.py
+++ Numbers/WorkingWithNumbers.py
@@ -4,3 +4,5 @@ print(4-9)#arithmetic Operation
 print(14*14)#Arithmetic Operation
 print(14*(2+9))#using parenthesis to specify the order of operation
 print(10 % 3)#using the modulus to get the remainder
+my_num = 79#using a variable to print out a value
+print(my_num)#prints out my_num
