839972eba735851b2fea34c913982f12b1c0acc2
NickBwalley <nickbwalley@gmail.com>
2020-05-22 03:52:01 +0300

    python

--- Strings/WorkingWithStrings.py
+++ Strings/WorkingWithStrings.py
@@ -1,2 +1,4 @@
 print("My name is NickBwalley\nI am a Coder") #\n the escape sequence used to print to a new line
 print("Also Called the \"Bronze_Bomber\"") #Prints a quotation mark on bronze Bomber word
+phrase = "PyCharm"
+print(phrase + " Is a cool IDE") #basically concatenations
