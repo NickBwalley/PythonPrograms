aa00324d710d8fa24d2864f32a0a0d7d82b6848e
NickBwalley <nickbwalley@gmail.com>
2020-05-22 04:33:17 +0300

    python

--- Numbers/WorkingWithNumbers.py
+++ Numbers/WorkingWithNumbers.py
@@ -2,3 +2,4 @@ print(99.99)#basic print statement
 print(79+99)#basic Arithmetic Operation
 print(4-9)#arithmetic Operation
 print(14*14)#Arithmetic Operation
+print(14*(2+9))#using parenthesis to specify the order of operation
