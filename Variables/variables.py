f1b85f340870902a04e06f620152bdd3b1c2b795
NickBwalley <nickbwalley@gmail.com>
2020-05-22 03:33:57 +0300

    python

--- Variables/variables.py
+++ Variables/variables.py
@@ -5,3 +5,10 @@ print("There was once a man named "+character_name + ",")
 print("He was " + character_age + " Years old")
 print("He really loved his name "+character_name)
 print("But he didn't love his age being "+character_age+"Years")
+print("***************************************")
+character_name = "Deontay Wilder"
+character_age = "35"
+print("There was once a man named "+character_name + ",")
+print("He was " + character_age + " Years old")
+print("He really loved his name "+character_name)
+print("But he didn't love his age being "+character_age+"Years")
