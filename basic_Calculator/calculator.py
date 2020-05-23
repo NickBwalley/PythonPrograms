15a882abfcebc830fc27d82dacf2254ff99fb9f8
NickBwalley <nickbwalley@gmail.com>
2020-05-23 03:12:22 +0300

    python

--- basic_Calculator/calculator.py
+++ basic_Calculator/calculator.py
@@ -2,4 +2,6 @@
 num1 = input("Enter A Number: ")
 num2 = input("Enter Another Number: ")
 result = num1 + num2
-print(result)
+result2 = int(num1) + int(num2)
+print(result)#prints inform of a string so answer will be incorrect
+print(result2)#prints out in form of an integer note only integers allowed otherwise returns an error
