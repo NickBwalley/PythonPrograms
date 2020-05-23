49e7f1f4072e914a36fc3aba6ad2d14648bc5be7
NickBwalley <nickbwalley@gmail.com>
2020-05-23 03:22:09 +0300

    python

---
+++ Madlibs_Game/madLibs_game.py
@@ -0,0 +1,4 @@
+
+print( "{name} is m best singer")
+print("My best rapper is {name}")
+print("Nelson Mandela won the {nobel_prize}")
--- basic_Calculator/calculator.py
+++ basic_Calculator/calculator.py
@@ -2,6 +2,8 @@
 num1 = input("Enter A Number: ")
 num2 = input("Enter Another Number: ")
 result = num1 + num2
-result2 = int(num1) + int(num2)
-print(result)#prints inform of a string so answer will be incorrect
-print(result2)#prints out in form of an integer note only integers allowed otherwise returns an error
+#result2 = int(num1) + int(num2)
+result3 = float(num1) + float(num2)
+#print(result)#prints inform of a string so answer will be incorrect
+#print(result2)#prints out in form of an integer note only integers allowed otherwise returns an error
+print(result3)#prints out in form of a float which has decimals
