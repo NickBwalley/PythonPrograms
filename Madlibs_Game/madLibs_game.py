ee5dce16a7ccea83ea321494b69b4986d7b322fd
NickBwalley <nickbwalley@gmail.com>
2020-05-23 03:25:13 +0300

    python

--- Madlibs_Game/madLibs_game.py
+++ Madlibs_Game/madLibs_game.py
@@ -1,4 +1,7 @@
 
-print( "{name} is m best singer")
-print("My best rapper is {name}")
-print("Nelson Mandela won the {nobel_prize}")
+singer_name = input("Enter Your Best Singer: ")
+rapper_name = input("Enter Your Best Rapper: ")
+
+print( "My best Singer is {singer_name}")
+print("My best rapper is {rapper_name}")
+print("My best WWE Wrestler is {wrestler_name} ")
