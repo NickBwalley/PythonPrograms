bbd74a13b020cc36b2774081d53ad88653df387c
NickBwalley <nickbwalley@gmail.com>
2020-05-25 00:05:42 +0300

    python

--- Lists/lists.py
+++ Lists/lists.py
@@ -1,4 +1,5 @@
 
 friends = ["Seth Rollins", "Brawn Strowman", "Alexa Bliss", "Aleister Black"]
 print (friends)#prints out the whole list
-print(friends[3])#prints out the third element of the list
+print(friends[0])#prints out the first element of the list
+print(friends[-1])#access elements from the last element
