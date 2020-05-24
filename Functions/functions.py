c95fe96be972d87c810127ab55215f54439fa04c
NickBwalley <nickbwalley@gmail.com>
2020-05-25 02:39:12 +0300

    python

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,6 +1,10 @@
 
-def say_hi (name):
-    print("Hello "+name)
+def say_hi (name, age, food, home):
+    print("Hello " + name + "\n You are "+age +" Years old"+
+    "\n You Love Eating "+food + "\n You also live in "+home)
 
-name = input("What is Your Name: ")
-say_hi(name)
+name = input("What is Your Name? ")
+age = input("What is Your Age? ")
+food = input("What is Your Favourite Meal? ")
+home = input("Where do you Stay?")
+say_hi(name, age, food, home)
