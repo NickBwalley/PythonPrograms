9ec7357154a7ea30d6cd31ca0fd207a04950c633
NickBwalley <nickbwalley@gmail.com>
2020-05-25 00:09:47 +0300

    pyton

--- Lists/lists.py
+++ Lists/lists.py
@@ -1,6 +1,7 @@
 
-friends = ["Seth Rollins", "Brawn Strowman", "Alexa Bliss", "Aleister Black"]
+friends = ["Seth", "Strowman", "Alexa", "Aleister", "Charlotte", "Becky"]
 print (friends)#prints out the whole list
 print(friends[0])#prints out the first element of the list
 print(friends[-1])#access elements from the last element
 print(friends[1:])#prints element from the 1st index
+print(friends[1:3])#prints value from one to three excluding three
