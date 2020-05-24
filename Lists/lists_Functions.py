6323aefa8b936fb6c8b22d114a60c27cf8915ff0
NickBwalley <nickbwalley@gmail.com>
2020-05-25 00:22:48 +0300

    python

--- Lists/lists_Functions.py
+++ Lists/lists_Functions.py
@@ -3,3 +3,7 @@ lucky_numbers = [79,99,599,789,779,799]
 
 friends.extend(lucky_numbers)#combines the first and the second list
 print(friends)
+friends.append("Undertaker")#adds another element at the end of the list
+print(friends)
+friends.insert(1,"Lana")#adds the element at the index1
+print(friends)
