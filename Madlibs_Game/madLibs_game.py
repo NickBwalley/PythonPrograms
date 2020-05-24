ba114ede1862b5948ba94596f4155f1c53a0400d
NickBwalley <nickbwalley@gmail.com>
2020-05-24 20:36:01 +0300

    python

--- Madlibs_Game/madLibs_game.py
+++ Madlibs_Game/madLibs_game.py
@@ -4,6 +4,6 @@ number = input("Enter Number of People with the Virus: ")
 country = input("Which country had the Most reported death in 24hours: ")
 
 print( "There was once in the year 2020 when the world was doomed!\n"
-       "A virus so called "+virus +" hit the wold in great shock!\n This disease took the lives of"
-       +str(number)+ ". The whole country faced a total lockdown with in some countries like "+country+
+       "A virus so called "+virus +" hit the wold in great shock!\n This disease took the lives of "
+       +number+ ". The whole country faced a total lockdown with in some countries like "+country+
        "\nhaving an estimate of 700Deaths reported in a single day a period of 24hours!")
