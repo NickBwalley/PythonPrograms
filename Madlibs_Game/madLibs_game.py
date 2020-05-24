39176fe7feaa5719c1196def6eeba2f07d674cf6
NickBwalley <nickbwalley@gmail.com>
2020-05-24 20:28:54 +0300

    python

--- Madlibs_Game/madLibs_game.py
+++ Madlibs_Game/madLibs_game.py
@@ -1,8 +1,9 @@
 
-comedian_name = input("Enter Your Best Singer: ")
-rapper_name = input("Enter Your Best Rapper: ")
-wrestler_name = input("Enter Your Best WWE Wrestler: ")
+virus = input("Enter Name of the Virus: ")
+number = input("Enter Number of People with the Virus: ")
+country = input("Which country had the Most reported death in 24hours: ")
 
-print( "My best Comedian is "+comedian_name)
-print("My best rapper is "+rapper_name)
-print("My best WWE Wrestler is "+wrestler_name)
+print( "There was once in the year 2020 when the world was doomed!\n"
+       "A virus so called "+virus +" hit the wold in great shock!\n This disease took the lives of"
+       +str(number)+ ". The whole country faced a total lockdown with in some countries like "+country+
+       "\nhaving an estimate of 700Deaths reported in a single day a period of 24hours!")
