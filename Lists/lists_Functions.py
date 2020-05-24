214197ae766a9e0eda305e6cd63f1a891d70a2df
NickBwalley <nickbwalley@gmail.com>
2020-05-25 00:19:05 +0300

    python

---
+++ Lists/lists_Functions.py
@@ -0,0 +1,5 @@
+friends = ["Lashley", "Goldberg", "McIntyre", "Wyatt", "Bayley", "Sasha"]
+lucky_numbers = [79,99,599,789,779,799]
+
+friends.extend(lucky_numbers)#combines the first and the second list
+print(friends)
