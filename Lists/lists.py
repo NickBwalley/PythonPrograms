f89161eb727466bf711511733262912ed9f25599
NickBwalley <nickbwalley@gmail.com>
2020-05-25 00:13:13 +0300

    python

--- Lists/lists.py
+++ Lists/lists.py
@@ -5,3 +5,5 @@ print(friends[0])#prints out the first element of the list
 print(friends[-1])#access elements from the last element
 print(friends[1:])#prints element from the 1st index
 print(friends[1:3])#prints value from one to three excluding three
+friends[0] = "Lashley" #value index 1 will be overridden
+print(friends[0:5])
