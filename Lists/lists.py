c35adef8c29a0adf8c32f06600bf9b9903aafca3
NickBwalley <nickbwalley@gmail.com>
2020-05-25 00:03:50 +0300

    python

---
+++ Lists/lists.py
@@ -0,0 +1,4 @@
+
+friends = ["Seth Rollins", "Brawn Strowman", "Alexa Bliss", "Aleister Black"]
+print (friends)#prints out the whole list
+print(friends[3])#prints out the third element of the list
