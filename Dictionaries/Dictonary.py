03fae43ad948ede19d84e71945cd5cc26d537960
NickBwalley <nickbwalley@gmail.com>
2020-05-26 00:37:21 +0300

    python

--- Dictionaries/Dictonary.py
+++ Dictionaries/Dictonary.py
@@ -10,8 +10,9 @@ monthsConversion = {
     "Sep": "September",
     "Oct": "October",
     "Nov": "November",
-    "Dec": "December",
+    "Dec": "December"
 }
 
 print (monthsConversion["Mar"])#First Way to Print Values from a dictionary using the key
 print(monthsConversion.get("Dec"))#Second way to Print Values from a Dictionary using the get()
+print(monthsConversion.get("Marse", "Invalid Key!"))#fails gracefully if invalid key is entered
