722f6e5d92bcd64c6a6192f9fe7e8da0d6383071
NickBwalley <nickbwalley@gmail.com>
2020-05-25 20:49:24 +0300

    python

--- If_Statements/If_Statements.py
+++ If_Statements/If_Statements.py
@@ -1,8 +1,10 @@
 
 is_male = True
 is_tall = False
-if is_male and is_tall:
-    print("You are a Male and Tall or Both Attributes!")
+if is_male and is_tall:#when using the and operator both conditions have to be true for the condition to return a true
+    print("You are a Tall Male!")
+elif is_male and not(is_tall):
+    print("You are a Short Male")
 else:
     print("You are Neither Male nor Tall!")
 
