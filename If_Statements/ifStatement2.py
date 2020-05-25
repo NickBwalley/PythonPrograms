4e4766b58f06d399bc5f39965384d382bc5e681a
NickBwalley <nickbwalley@gmail.com>
2020-05-25 21:35:39 +0300

    python

---
+++ If_Statements/ifStatement2.py
@@ -0,0 +1,7 @@
+#function
+def eligible (age):
+    if age >= 18:
+       print("Welcome to Beers Point!... You can order any drinks...")
+    else:
+       print("You are Stil a Kid!... Go Back to School and Learn!...")
+eligible (19)
