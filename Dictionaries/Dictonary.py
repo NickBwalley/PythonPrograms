cf99b1b492a582bdc76446ba43805aab078b00b6
NickBwalley <nickbwalley@gmail.com>
2020-05-26 00:39:39 +0300

    python

--- Dictionaries/Dictonary.py
+++ Dictionaries/Dictonary.py
@@ -1,4 +1,4 @@
-
+#Dictionaries can be useful to store Key value pairs 
 monthsConversion = {
     "Jan": "January",
     "Feb": "February",
@@ -10,9 +10,10 @@ monthsConversion = {
     "Sep": "September",
     "Oct": "October",
     "Nov": "November",
-    "Dec": "December"
+    "Dec": "December",
+    1: "Sunday"
 }
 
 print (monthsConversion["Mar"])#First Way to Print Values from a dictionary using the key
 print(monthsConversion.get("Dec"))#Second way to Print Values from a Dictionary using the get()
-print(monthsConversion.get("Marse", "Invalid Key!"))#fails gracefully if invalid key is entered
+print(monthsConversion.get(1, "Invalid Key!"))#Default Value if invalid key is entered
