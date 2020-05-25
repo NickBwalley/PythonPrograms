e619045d792ecbc78a4732a6b7a28e81ae07bf18
NickBwalley <nickbwalley@gmail.com>
2020-05-26 00:32:03 +0300

    python

---
+++ Dictionaries/Dictonary.py
@@ -0,0 +1,16 @@
+
+monthsConversion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print (monthsConversion["Mar"])#First Way to Print Values from a dictionary using the key
