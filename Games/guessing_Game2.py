4853081f4973c3d8cac77050c20ed26364738799
NickBwalley <nickbwalley@gmail.com>
2020-05-27 20:50:48 +0300

    python

--- Games/guessing_Game2.py
+++ Games/guessing_Game2.py
@@ -1,20 +1,10 @@
-secret_word = "diamond"
+secret_word = "Silver"
 guess = ""
 guess_count = 0
 guess_limit = 3
 out_of_guesses = False
-print("*********************")
-print("This is a Guessing Game\nYou have 3Trials to guess\nThe Secret Word")
-print("*********************")
-
-while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter Your Guess: ")
-        guess_count +=1
-    else:
-        out_of_guesses = True
-
-if out_of_guesses:
-    print("Out of Guesses, YOU LOSE!..")
-else:
-    print("YOU WIN!..")
+print("----------------------------")
+print("Guessing Game: Hint(Arg)")
+print("----------------------------")
+while guess != secret_word:
+    guess = input("")
