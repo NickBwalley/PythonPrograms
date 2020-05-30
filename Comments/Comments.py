05998bb7863ef55112ae64357f55912794c6ff68
NickBwalley <nickbwalley@gmail.com>
2020-05-31 01:47:56 +0300

    python

--- Comments/Comments.py
+++ Comments/Comments.py
@@ -1,4 +1,9 @@
 #This is just a commented line
+'''
+This is just another comment section todays date is 31-05-2020
+This corona Virus pandemic has shocked the world has its cure has never been found
+This also has caused alot of people to loose their lives as well as drop the entire worlds economy
+'''
 def print_Crap(name):
     return name
 name = (input("What is Your Name: "))
