f2670c82f88e72ca4a9c4557eef8464f923b2afe
NickBwalley <nickbwalley@gmail.com>
2020-05-31 01:50:40 +0300

    python

--- Comments/Comments.py
+++ Comments/Comments.py
@@ -4,6 +4,11 @@ This is just another comment section todays date is 31-05-2020
 This corona Virus pandemic has shocked the world has its cure has never been found
 This also has caused alot of people to loose their lives as well as drop the entire worlds economy
 '''
+#Two common uses for comments in code
+'''
+To write out short notes of a certain line of code
+To comment out a suspected erroneous line of code
+'''
 def print_Crap(name):
     return name
 name = (input("What is Your Name: "))
