93f106bd7cbb83ad48c6efe72384129b8c146bb2
NickBwalley <nickbwalley@gmail.com>
2020-05-31 01:04:57 +0300

    python

--- 2DListsAndNestedLoops/2DLists.py
+++ 2DListsAndNestedLoops/2DLists.py
@@ -10,3 +10,10 @@ print(number_list[0][0])#prints out 1
 print(number_list[0][1])#prints out 2
 print(number_list[1][1])#prints out 5
 print(number_list[3][0])#prints out 0
+#Using a NestedLoop to print the elements in the 2DList
+for rows in number_list:
+    print(rows)#prints out all the values inside the number_list
+#printing out the values of the list individually
+for rows in number_list:
+    for index in rows:
+        print(index)#prints out the elements individually
