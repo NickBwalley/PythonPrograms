0ad124f38ef797b98f528cbc40c8c8bef386a178
NickBwalley <nickbwalley@gmail.com>
2020-05-31 00:59:08 +0300

    python

---
+++ 2DListsAndNestedLoops/2DLists.py
@@ -0,0 +1,12 @@
+#creating a 2D list
+number_list = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+#Accessing the elements inside the list
+print(number_list[0][0])#prints out 1
+print(number_list[0][1])#prints out 2
+print(number_list[1][1])#prints out 5
+print(number_list[3][0])#prints out 0
