0a242a4ad5c7aa8e77696c9219bc1573ec3c15c8
NickBwalley <nickbwalley@gmail.com>
2020-05-31 00:44:09 +0300

    python

--- Exponent_Function/ExponentFunction.py
+++ Exponent_Function/ExponentFunction.py
@@ -1,2 +1,11 @@
 
 print(2**3) #same as 2^3
+
+#function to raise_number_to_power
+def raise_to_power(base_num, power_num):
+    result = 1
+    for index in range(power_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 3))#from function raise_to_power
