f5b2ccb0d2b41427dd3b2bef256eed02ab67096a
NickBwalley <nickbwalley@gmail.com>
2020-05-30 07:29:00 +0300

    python

--- ForLoop/forLoop.py
+++ ForLoop/forLoop.py
@@ -1,6 +1,9 @@
 
 for letters in "Prickler Scurry":
-    print(letters)
+    print(letters)#prints each letter individually
 friends = ["Scurry", "Dray", "Duzzler", "Prickler"]
 for friend in friends:
-    print(friend)
+    print(friend)#prints out the elements in the array
+
+for index in range(10):#prints out the values from 1 >> 10
+    print(index)
