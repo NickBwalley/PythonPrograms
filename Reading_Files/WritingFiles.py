258a4841669400ba01c3230dc6f7998339a8a34c
NickBwalley <nickbwalley@gmail.com>
2020-05-31 03:25:24 +0300

    python

--- Reading_Files/WritingFiles.py
+++ Reading_Files/WritingFiles.py
@@ -5,8 +5,8 @@ The 'a' is the appropriate as it adds the file at the end of your text lines
 NOTE!...
 When you are using the 'w' it can also create a new file that is if it is not there
 '''
-employee_file = open("index2.txt", "w")
-employee_file.write("\nBobby Lashley - Almighty")#adding an item inside the writingFiles
+employee_file = open("Scurry.html", "w")
+employee_file.write("<p>This is NickBwalley the Greatest and Coolest programmer</p>")#adding an item inside the writingFiles
 
 
 employee_file.close()
