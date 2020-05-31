665f13d6228a41e637122f8f42ead564a65fbb76
NickBwalley <nickbwalley@gmail.com>
2020-05-31 21:44:28 +0300

    python

--- ModulesAndPips/Modules.py
+++ ModulesAndPips/Modules.py
@@ -3,3 +3,5 @@ import useful_Tools
 
 print(useful_Tools.My_Name("NickBwalley"))
 print(useful_Tools.feet_to_centimeters)
+
+print(useful_Tools.print_vowels("Nick"))
--- ModulesAndPips/useful_Tools.py
+++ ModulesAndPips/useful_Tools.py
@@ -1,13 +1,17 @@
-import random
-
 
 feet_to_inches = 12
 feet_to_centimeters = 31
 
-def Roll_Dice(num):
-    return random.radint(1, num)
-
 def My_Name(name):
     return name
+
+
+
+def print_vowels(name):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for letter in name:
+        if letter in vowels:
+            print(letter)
+
 
 
