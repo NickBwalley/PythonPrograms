3f94b12629e0b62b9f6ea5f5b21ed2038bef7529
NickBwalley <nickbwalley@gmail.com>
2020-06-01 00:48:31 +0300

    pyton

--- Classes_Objects/Prickler.py
+++ Classes_Objects/Prickler.py
@@ -1,7 +1,10 @@
 from Scurry import Student
 
 Student1 = Student("Nicholas", 122790, 6.1, False)
-print(Student1.name)
+Student2 = Student("Lorna", 122994, 6.0, False)
+print(Student1.name)#an object of the students class
 print(Student1.stdNo)
 print(Student1.gpa)
 print(Student1.is_on_probation)
+print("--------------")
+print(Student2.name)#different object of the Student class
