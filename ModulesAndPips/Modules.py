17e2afd5ed70486558d0d7a71da16a987b1942b3
NickBwalley <nickbwalley@gmail.com>
2020-05-31 21:33:33 +0300

    python

---
+++ ModulesAndPips/Modules.py
@@ -0,0 +1,5 @@
+import useful_Tools
+
+
+print(useful_Tools.My_Name("NickBwalley"))
+print(useful_Tools.feet_to_centimeters)
---
+++ ModulesAndPips/useful_Tools.py
@@ -0,0 +1,13 @@
+import random
+
+
+feet_to_inches = 12
+feet_to_centimeters = 31
+
+def Roll_Dice(num):
+    return random.radint(1, num)
+
+def My_Name(name):
+    return name
+
+
