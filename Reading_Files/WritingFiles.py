024c91bba4f7f3ac84c39f8a9c5da1eeb05240c0
NickBwalley <nickbwalley@gmail.com>
2020-05-31 03:31:24 +0300

    python

--- Reading_Files/WritingFiles.py
+++ Reading_Files/WritingFiles.py
@@ -15,6 +15,4 @@ When you are using the 'w' it can also create a new file that is if it is not th
 '''
 employee_file = open("Scurry.html", "w")
 #employee_file.write("<p>This is NickBwalley the Greatest and Coolest programmer</p>")#adding an item inside the writingFiles
-
-
-employee_file.close()
+employee_file.close()#good python practice to close files after opening
