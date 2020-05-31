4e8ea3d8303ea1c5076bda7489155e96a6577ec5
NickBwalley <nickbwalley@gmail.com>
2020-05-31 03:20:11 +0300

    python

--- Reading_Files/WritingFiles.py
+++ Reading_Files/WritingFiles.py
@@ -1,4 +1,8 @@
-
+'''
+WARNING!...
+The 'w' will override the whole text and insert the newly appended text inside
+The 'a' is the appropriate as it adds the file at the end of your text lines
+'''
 employee_file = open("index.txt", "a")
 #employee_file.write("\nBobby Lashley - Almighty")#adding an item inside the writingFiles
 
