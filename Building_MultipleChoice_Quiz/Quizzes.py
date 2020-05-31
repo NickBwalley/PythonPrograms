fe1ce50e3d462cca63b58b05a76bdd9529510b4f
NickBwalley <nickbwalley@gmail.com>
2020-06-01 01:11:59 +0300

    python

---
+++ Building_MultipleChoice_Quiz/Quizzes.py
@@ -0,0 +1,8 @@
+
+question_prompts = [
+    "A group of Squirrels are Called? \n(a)Drury\n(b)Spike\n(d)Scarlet\n(d)Scurry\n",
+    "A group of Monkeys are Called? \n(a)Scurry\n(b)Bunch\n(d)Scree\n(d)Troop\n",
+    "A group of Giraffes are Called? \n(a)Scurry\n(b)Prickle\n(d)Tower\n(d)Toasters\n",
+    "A group of Pocupines are Called? \n(a)Shoal\n(b)Troop\n(d)Prickle\n(d)Dazzle\n",
+
+]
