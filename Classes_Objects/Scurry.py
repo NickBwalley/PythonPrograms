0e911dc305d79f7ea41ad2991120c787f8cdb5e8
NickBwalley <nickbwalley@gmail.com>
2020-06-01 00:35:24 +0300

    python

--- Classes_Objects/Scurry.py
+++ Classes_Objects/Scurry.py
@@ -1,3 +1,7 @@
 class Student:
 
     def __init__(self, name, stdNo, gpa, is_on_probation):
+        self.name = name
+        self.stdNo = stdNo
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
