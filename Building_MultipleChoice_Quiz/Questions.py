301a5bf9210e3831ecbffde6aae9b521f5aeb937
NickBwalley <nickbwalley@gmail.com>
2020-06-01 01:52:02 +0300

    python

--- Building_MultipleChoice_Quiz/Questions.py
+++ Building_MultipleChoice_Quiz/Questions.py
@@ -1,4 +1,4 @@
-class Questions:
+class Question:
     def __int__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
--- Building_MultipleChoice_Quiz/Quizzes.py
+++ Building_MultipleChoice_Quiz/Quizzes.py
@@ -1,17 +1,27 @@
-from Questions import Questions
+from Questions import Question
 
 question_prompts = [
-    "A group of Squirrels are Called? \n(a)Drury\n(b)Spike\n(c)Scarlet\n(d)Scurry\n",
-    "A group of Monkeys are Called? \n(a)Scurry\n(b)Troop\n(c)Scree\n(d)Bunch\n",
-    "A group of Giraffes are Called? \n(a)Tower\n(b)Prickle\n(c)Scurry\n(d)Toasters\n",
-    "A group of Pocupines are Called? \n(a)Shoal\n(b)Troop\n(c)Prickle\n(d)Dazzle\n",
+    "A group of Squirrels are Called?\n(a)Drury\n(b)Spike\n(c)Scarlet\n(d)Scurry\n\n",
+    "A group of Monkeys are Called?\n(a)Scurry\n(b)Troop\n(c)Scree\n(d)Bunch\n\n",
+    "A group of Giraffes are Called?\n(a)Tower\n(b)Prickle\n(c)Scurry\n(d)Toasters\n\n",
+    "A group of Porcupines are Called?\n(a)Shoal\n(b)Troop\n(c)Prickle\n(d)Dazzle\n\n"
 ]
 
 questions = [
-    Questions(question_prompts[0], "d"),
-    Questions(question_prompts[1], "b"),
-    Questions(question_prompts[2], "a"),
-    Questions(question_prompts[3], "c"),
+    Question(question_prompts[0], "d"),
+    Question(question_prompts[1], "b"),
+    Question(question_prompts[2], "a"),
+    Question(question_prompts[3], "c"),
 
 ]
+
+def run_test(questions):
+    score = 0
+    for qtn in questions:
+        answer = questions.prompt
+        if answer == questions.answer:
+            score += 1
+    print("You Scored: " + str(score) + "/" + str(len(questions)))
+
+run_test(questions)
 
