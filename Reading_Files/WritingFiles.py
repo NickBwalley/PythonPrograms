f3247b7b281d4e1cbfcedd1ceba7d996cd45e28e
NickBwalley <nickbwalley@gmail.com>
2020-05-31 03:22:20 +0300

    python

--- Reading_Files/WritingFiles.py
+++ Reading_Files/WritingFiles.py
@@ -2,9 +2,11 @@
 WARNING!...
 The 'w' will override the whole text and insert the newly appended text inside
 The 'a' is the appropriate as it adds the file at the end of your text lines
+NOTE!...
+When you are using the 'w' it can also create a new file that is if it is not there
 '''
-employee_file = open("index.txt", "a")
-#employee_file.write("\nBobby Lashley - Almighty")#adding an item inside the writingFiles
+employee_file = open("index2.txt", "w")
+employee_file.write("\nBobby Lashley - Almighty")#adding an item inside the writingFiles
 
 
 employee_file.close()
