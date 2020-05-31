40900fc6768508f8a9c06c9797006d097e47ac3b
NickBwalley <nickbwalley@gmail.com>
2020-06-01 01:18:53 +0300

    python

--- Building_MultipleChoice_Quiz/Quizzes.py
+++ Building_MultipleChoice_Quiz/Quizzes.py
@@ -1,8 +1,17 @@
+from Questions import Questions
 
 question_prompts = [
-    "A group of Squirrels are Called? \n(a)Drury\n(b)Spike\n(d)Scarlet\n(d)Scurry\n",
-    "A group of Monkeys are Called? \n(a)Scurry\n(b)Bunch\n(d)Scree\n(d)Troop\n",
-    "A group of Giraffes are Called? \n(a)Scurry\n(b)Prickle\n(d)Tower\n(d)Toasters\n",
-    "A group of Pocupines are Called? \n(a)Shoal\n(b)Troop\n(d)Prickle\n(d)Dazzle\n",
+    "A group of Squirrels are Called? \n(a)Drury\n(b)Spike\n(c)Scarlet\n(d)Scurry\n",
+    "A group of Monkeys are Called? \n(a)Scurry\n(b)Troop\n(c)Scree\n(d)Bunch\n",
+    "A group of Giraffes are Called? \n(a)Tower\n(b)Prickle\n(c)Scurry\n(d)Toasters\n",
+    "A group of Pocupines are Called? \n(a)Shoal\n(b)Troop\n(c)Prickle\n(d)Dazzle\n",
+]
+
+questions = [
+    Questions(question_prompts[0], "d"),
+    Questions(question_prompts[1], "b"),
+    Questions(question_prompts[2], "a"),
+    Questions(question_prompts[3], "c"),
 
 ]
+
