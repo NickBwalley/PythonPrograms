75114a0bece4241a8a185e08ae2a53d7d635d0a4
NickBwalley <nickbwalley@gmail.com>
2020-05-31 03:29:19 +0300

    python

--- Reading_Files/WritingFiles.py
+++ Reading_Files/WritingFiles.py
@@ -5,8 +5,16 @@ The 'a' is the appropriate as it adds the file at the end of your text lines
 NOTE!...
 When you are using the 'w' it can also create a new file that is if it is not there
 '''
+#concepts
+'''
+'a' -> append an element inside a file 
+'w' -> write an element inside a file which means that it will override the existing one
+'w' -> creates a new file if the file does not exist and adds the new element 
+'r' -> Reads a file and prints it out 
+'r+' -> Reads and Writes a file inside a new file 
+'''
 employee_file = open("Scurry.html", "w")
-employee_file.write("<p>This is NickBwalley the Greatest and Coolest programmer</p>")#adding an item inside the writingFiles
+#employee_file.write("<p>This is NickBwalley the Greatest and Coolest programmer</p>")#adding an item inside the writingFiles
 
 
 employee_file.close()
