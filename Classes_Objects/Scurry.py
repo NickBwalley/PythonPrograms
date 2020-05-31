3f76c1084a2f3222786ee26ea2aff40f3f5890b4
NickBwalley <nickbwalley@gmail.com>
2020-06-01 00:54:56 +0300

    python

--- Classes_Objects/Scurry.py
+++ Classes_Objects/Scurry.py
@@ -1,5 +1,6 @@
-class Student:
-
+class Student:#Creating a Students class
+    #creating a student data-type(user-defined)
+    #we can model real-world objects and we can create data-types
     def __init__(self, name, stdNo, gpa, is_on_probation):
         self.name = name
         self.stdNo = stdNo
