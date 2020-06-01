b74232ec79cfc9dcc211f3f159a5c3a7d4b987b4
NickBwalley <nickbwalley@gmail.com>
2020-06-01 05:32:57 +0300

    python

--- Object_Functions/NickFury.py
+++ Object_Functions/NickFury.py
@@ -1,4 +1,9 @@
 from Students import Student
 
-Student1 = Student("Nick", 122790, "DBIT", False)
-print(Student1.name + " "+ str(Student1.stdNo) + " " + Student1.course)
+Student1 = Student("Nick-Bwalley", 122790, "DBIT", 2.9)
+Student2 = Student("Drew Mc-Intyre", 122994, "DBIT",3.6)
+Student3 = Student("Seth Rollins", 122799, "DBIT",3.9)
+
+print(Student1.on_honor_roll())
+print(Student2.on_honor_roll())
+print(Student3.on_honor_roll())
--- Object_Functions/Students.py
+++ Object_Functions/Students.py
@@ -1,6 +1,12 @@
 class Student:
-    def __init__(self, name, stdNo, course, is_on_probation):
+    def __init__(self, name, stdNo, course, gpa):
         self.name = name
         self.stdNo = stdNo
         self.course = course
-        self.is_on_operation = is_on_probation
+        self.gpa = gpa
+
+    def on_honor_roll (self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
