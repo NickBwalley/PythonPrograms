65cb5ab1f5dc8f82ae6704ea69f7eb0b24d89849
NickBwalley <nickbwalley@gmail.com>
2020-06-01 05:21:46 +0300

    python

---
+++ Object_Functions/NickFury.py
@@ -0,0 +1,4 @@
+from Students import Student
+
+Student1 = Student("Nick", 122790, "DBIT", False)
+print(Student1.name + " "+ str(Student1.stdNo))
---
+++ Object_Functions/Students.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, stdNo, course, is_on_probation):
+        self.name = name
+        self.stdNo = stdNo
+        self.course = course
+        self.is_on_operation = is_on_probation
