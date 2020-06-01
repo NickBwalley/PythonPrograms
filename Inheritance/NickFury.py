1eeb9fe0b95e3f499486a4309d25dc3ac5c3fa7f
NickBwalley <nickbwalley@gmail.com>
2020-06-01 05:41:50 +0300

    python

---
+++ Inheritance/NickFury.py
@@ -0,0 +1,4 @@
+from SpecialFood import Food
+
+myFood = Food()
+myFood.make_chicken()
---
+++ Inheritance/SpecialFood.py
@@ -0,0 +1,9 @@
+class Food:
+    def make_chicken(self):
+        print("Chicken made by Chef: Aaron Spears")
+
+    def make_salad(self):
+        print("Salad made by Chef: Michael James")
+
+    def make_beef(self):
+        print("beef made by Chef: TomAndJerry")
