e1aecf3104bdc2882ac918ed940f69ff1cf2f7ab
NickBwalley <nickbwalley@gmail.com>
2020-06-01 05:26:37 +0300

    python

--- Object_Functions/NickFury.py
+++ Object_Functions/NickFury.py
@@ -1,4 +1,4 @@
 from Students import Student
 
 Student1 = Student("Nick", 122790, "DBIT", False)
-print(Student1.name + " "+ str(Student1.stdNo))
+print(Student1.name + " "+ str(Student1.stdNo) + " " + Student1.course)
