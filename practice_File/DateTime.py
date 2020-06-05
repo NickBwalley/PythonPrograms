0800ebea47b27c16943fd7fd4c6b270e938b4f84
NickBwalley <nickbwalley@gmail.com>
2020-06-05 04:00:27 +0300

    python

--- practice_File/DateTime.py
+++ practice_File/DateTime.py
@@ -8,8 +8,9 @@ odds = [1,3,5,7,9,11,13,15,17,19,21,
 47,49,51,53,55,57,59
 ]
 
-right_this_minute = datetime.today().minute
+
 for i in range(5):
+    right_this_minute = datetime.today().minute
     if right_this_minute in odds:
         print("This is an Odd Minute!..")
     else:
