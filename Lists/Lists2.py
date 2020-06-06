4834c616a4224df3bc6394ac9b08d5dde79ddc25
NickBwalley <nickbwalley@gmail.com>
2020-06-06 05:10:23 +0300

    python

--- Lists/Lists2.py
+++ Lists/Lists2.py
@@ -1,5 +1,6 @@
 vowels = ['a','e','i','o','u']
-name = input("What is your Name? ")
+#name = input("What is your Name? ")
+name = "Nicholas Bwalley"
 found = [] #creating an empty list
 for letter in name:
     if letter in vowels:
@@ -8,3 +9,19 @@ for letter in name:
 
 for vowel in found:
     print(vowel)
+print("-----------------")
+'''
+Notes:
+1. remove -> Takes an objects value as its sole argument
+2. pop -> Takes an optional index as its arguments 
+3. extends -> Takes a list objects as its sole arguments 
+4. Insert -> Takes an index valuee and an object as its sole arguments 
+5. 
+'''
+
+phrase = "Don't Panic!"
+print(phrase)
+plist = list(phrase)
+print(plist)
+new_phrase = ''.join(plist)
+print(new_phrase)
