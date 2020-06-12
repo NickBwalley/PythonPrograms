ce5f4c2a4bfe3137a98df072fc22fc20acc931fa
NickBwalley <nickbwalley@gmail.com>
2020-06-12 06:09:26 +0300

    python

--- Bubble_Sort/bubbleSort.py
+++ Bubble_Sort/bubbleSort.py
@@ -1,18 +1,8 @@
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range (0, n-i-1):
-            if(arr[j] > arr[j+1]):
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+#function to sort an array using bubbleSort
 
-arr = [949,232,676,590,844,331,991];
-#print out the array before being sorted
-print("The Array Elements before sorting: ")
-for i in range (len(arr)):
+
+arr = [466,979,549,898,344,561,663,199,79];
+z = len(arr)
+print("The Array Before Sorting is: ")
+for i in range(z):
     print(arr[i])
-#sorting the Array
-bubbleSort(arr);
-#print out the Array elements after being sorted
-print("The Array after being sorted is: ")
-for j in range(len(arr)):
-    print(arr[j])
