c3b7281596409ff214818afac8cabb151040b9c0
NickBwalley <nickbwalley@gmail.com>
2020-06-12 06:13:06 +0300

    java

--- Bubble_Sort/bubbleSort.py
+++ Bubble_Sort/bubbleSort.py
@@ -1,4 +1,10 @@
 #function to sort an array using bubbleSort
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if(arr[j] > arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
 arr = [466,979,549,898,344,561,663,199,79];
@@ -6,3 +12,7 @@ z = len(arr)
 print("The Array Before Sorting is: ")
 for i in range(z):
     print(arr[i])
+print("The Array After Sorting is: ")
+bubbleSort(arr)
+for j in range(z):
+    print(arr[j])
