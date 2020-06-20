a30da1b4ee49c7641ffc88cf08fb0e2f864f1421
NickBwalley <nickbwalley@gmail.com>
2020-06-20 03:54:21 +0300

    java

--- Bubble_Sort/NickFury.py
+++ Bubble_Sort/NickFury.py
@@ -1,7 +1,3 @@
-#Function to sort an Array using bubbleSort
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if(arr[j] < arr[j+1]):
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+class sortArray:
+    #sorting an array using bubbleSort
+    
---
+++ Bubble_Sort/bubbleSort.py
@@ -0,0 +1,9 @@
+from NickFury import sortArray
+
+myArray = sortArray()
+
+arr = [943,233,344,933,798,90]
+n = len(arr)
+finalArr = myArray.bubbleSort(n)
+
+
