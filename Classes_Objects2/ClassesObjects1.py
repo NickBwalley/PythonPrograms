dd16ea988d10672bc6843e4bab887963534ccd1c
NickBwalley <nickbwalley@gmail.com>
2020-06-25 15:36:54 +0300

    python

--- Classes_Objects2/ClassesObjects1.py
+++ Classes_Objects2/ClassesObjects1.py
@@ -1,18 +1,23 @@
 class student:
-    def __init__(self, first, last, email, std_no):
-        self.first = first;
-        self.last = last;
+    def __init__(self, first, last, std_no):
+        self.first = first
+        self.last = last
         self.email = first + '.' + last + '@strathmore.edu'
         self.std_no = std_no
+    def full_name(self):
+        return '{} {}'.format(self.first, self.last)
 
-student_1 = student()
-student_2 = student()
 
-print (student_1)
-print (student_2)
 
-#first_method
-student_1.first = ('Nicholas')
-student_1.last = ('Bwalley')
-student_1.email = ('Nicholas.Bwalley@strathmore.edu')
-student_1.std_no = (122790)
+student_1 = student('Nicholas', 'Bwalley', 122790)
+print(student_1.full_name())
+#student_2 = student()
+
+#print (student_1)
+#print (student_2)
+
+# first_method
+# student_1.first = ('Nicholas')
+# student_1.last = ('Bwalley')
+# student_1.email = ('Nicholas.Bwalley@strathmore.edu')
+# student_1.std_no = (122790)
