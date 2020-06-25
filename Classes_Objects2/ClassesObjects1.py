c21f652c5a0d2465df1b740f8101ed1279179bf2
NickBwalley <nickbwalley@gmail.com>
2020-06-25 15:00:24 +0300

    python

---
+++ Classes_Objects2/ClassesObjects1.py
@@ -0,0 +1,14 @@
+class student:
+    pass
+student_1 = student()
+student_2 = student()
+
+print (student_1)
+print (student_2)
+
+student_1.first = ('Nicholas')
+student_1.last = ('Bwalley')
+student_1.email = ('Nicholas.Bwalley@strathmore.edu')
+student_1.stdNo = (122790)
+
+print(student_1.email)
