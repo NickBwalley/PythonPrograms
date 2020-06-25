7534a2135a66dc5349750032c6db43463e9981e2
NickBwalley <nickbwalley@gmail.com>
2020-06-25 15:15:31 +0300

    pyton

--- Classes_Objects2/ClassesObjects1.py
+++ Classes_Objects2/ClassesObjects1.py
@@ -1,14 +1,18 @@
 class student:
-    pass
+    def __init__(self, first, last, email, std_no):
+        self.first = first;
+        self.last = last;
+        self.email = first + '.' + last + '@' + 'strathmore.edu'
+        self.std_no = std_no
+        
 student_1 = student()
 student_2 = student()
 
 print (student_1)
 print (student_2)
 
+#first_method
 student_1.first = ('Nicholas')
 student_1.last = ('Bwalley')
 student_1.email = ('Nicholas.Bwalley@strathmore.edu')
-student_1.stdNo = (122790)
-
-print(student_1.email)
+student_1.std_no = (122790)
