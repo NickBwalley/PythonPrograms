9a9a9d1016025c3353b4d435fc8217ff89a47764
NickBwalley <nickbwalley@gmail.com>
2020-06-26 12:37:43 +0300

    python

--- python_constructors/python_constructors.py
+++ python_constructors/python_constructors.py
@@ -15,3 +15,32 @@ fighter_3 = heavyWeights("Mike Tyson", 65, 5, 1)
 
 print (heavyWeights.the_legends(fighter_1))
 print(fighter_1.the_true_legends())
+print("----------------------------------")
+
+# Counting the number of objects created from a Class
+class Students:
+    count = 0
+    def __init__(self):
+        Students.count = Students.count + 1
+
+s1 = Students()
+s2 = Students()
+s3 = Students()
+
+print("Number of Students are: ", Students.count)
+print("----------------------------------")
+
+# PYTHON NON-PARAMETERISED CONSTRUCTOR
+'''
+The non-parameterized constructor uses when we do not want to manipulate the value 
+or the constructor that has only self as an argument. Consider the following example.
+'''
+class Student:
+    def __init__(self):
+        print("This is a Non-Parameterised Constructor")
+
+    def students_name(self,name):
+        print("Hello", name)
+
+myStudent = Student()
+myStudent.students_name("Nicholas")
