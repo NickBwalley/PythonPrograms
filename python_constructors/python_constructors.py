919f7738a511d99cfa9956c14f2c7523452fe80f
NickBwalley <nickbwalley@gmail.com>
2020-06-26 12:24:25 +0300

    python

---
+++ python_constructors/python_constructors.py
@@ -0,0 +1,16 @@
+class heavyWeights:
+    def __init__(self, name, wins, loss, draw):
+        self.name = name
+        self.wins = wins
+        self.loss = loss
+        self.draw = draw
+    def the_legends(self):
+        return '{} {} {} {}'.format(self.name, self.wins, self.loss, self.draw)
+    def the_true_legends(self):
+        return 
+
+fighter_1 = heavyWeights("Floyd Mayweather", 50, 0, 0)
+fighter_2 = heavyWeights("Deontay Wilder", 42, 1, 1)
+fighter_3 = heavyWeights("Mike Tyson", 65, 5, 1)
+
+print (heavyWeights.the_legends(fighter_1))
