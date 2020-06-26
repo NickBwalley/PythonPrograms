1aaedf19f5fbc1da2dcb270b2e4a015d3d9eb432
NickBwalley <nickbwalley@gmail.com>
2020-06-26 12:28:32 +0300

    python

--- python_constructors/python_constructors.py
+++ python_constructors/python_constructors.py
@@ -7,10 +7,11 @@ class heavyWeights:
     def the_legends(self):
         return '{} {} {} {}'.format(self.name, self.wins, self.loss, self.draw)
     def the_true_legends(self):
-        return 
+        return "Name: %s \t Wins: %d \t Loss: %d \t Draw: %d \t"%(self.name, self.wins,self.loss, self.draw)
 
 fighter_1 = heavyWeights("Floyd Mayweather", 50, 0, 0)
 fighter_2 = heavyWeights("Deontay Wilder", 42, 1, 1)
 fighter_3 = heavyWeights("Mike Tyson", 65, 5, 1)
 
 print (heavyWeights.the_legends(fighter_1))
+print(fighter_1.the_true_legends())
