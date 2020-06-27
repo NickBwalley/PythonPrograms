a4522e8f4e523f37b0568a6d26974b7d2b674b73
NickBwalley <nickbwalley@gmail.com>
2020-06-27 06:21:05 +0300

    python

---
+++ TKINTER/introduction.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root = Tk() # creating a blank Window
+first_label = Label(root, text="Welcome to Python Tkinter") # creating a label
+first_label.pack() # pack everything inside the window and display
+first_label.mainloop() # so that the output constantly displays until the user decides to close the window
