fd91d598c507d48c1f48af6d44700e9bd705def7
NickBwalley <nickbwalley@gmail.com>
2020-06-29 20:44:30 +0300

    python

--- OOP_CONCEPTS/Inheritance.py
+++ OOP_CONCEPTS/Inheritance.py
@@ -90,3 +90,20 @@ The isinstance (obj, class) method
 The isinstance() method is used to check the relationship between the objects and classes. 
 It returns true if the first parameter, i.e., obj is the instance of the second parameter, i.e., class.
 '''
+class Calculation1:
+    def summation(self, a, b):
+        return a + b
+
+class Calculation2:
+    def subtraction(self, a, b):
+        return a - b
+
+class Derived(Calculation2, Calculation1):
+    def division(self, a, b):
+        return a/b
+    def multiplication(self, a, b):
+        return a*b
+
+d = Derived()
+print(isinstance(d, Derived)) # prints true if the object d is an instance of the class Derived
+print(isinstance(d, Calculation3)) # prints false
