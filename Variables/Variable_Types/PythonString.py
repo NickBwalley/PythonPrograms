5243d3bd86aa914b864ed04bac2a84959f32ae38
NickBwalley <nickbwalley@gmail.com>
2020-07-14 14:01:20 +0300

    Python

---
+++ Variables/Variable_Types/PythonString.py
@@ -0,0 +1,9 @@
+string = "NickBwalley"
+print (string) # Prints 'NickBwalley'
+print(string[0]) # Prints 'N'
+print(string[2:5]) # Prints 'ckB'
+print(string[2:]) # prints string starting from the third character
+print(string * 2) # prints string two times in one time
+print(string + "BIIY") # prints the string concatenation operator 'NickBwalleyBIIY'
+print(string[:3]) # Prints 'Nic'
+print(string[:-1]) # prints reverse
