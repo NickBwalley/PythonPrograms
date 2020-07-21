8b84bba9f0ea8134837ca517b3e7a019ee335da8
NickBwalley <nickbwalley@gmail.com>
2020-07-21 14:22:15 +0300

    Python

---
+++ Python_Basic_Operators/BasicOperators.py
@@ -0,0 +1,14 @@
+# Python_Basic_Operators
+# a and b are variables
+a = 10
+b = 20
+# Arithmetic Operator
+print(a+b) # Addition
+print(a-b) # Subtraction
+print(a/b) # Division
+print(a*b) # Multiplication
+print(b%a) # modulus
+print(b**a) # Exponent (b=20 to the power a=10)
+a = 9
+b = 2
+print(a//b) # Floor Division
