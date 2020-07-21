805814ba8769a23b03a0fdbab94e90026e140508
NickBwalley <nickbwalley@gmail.com>
2020-07-21 14:38:25 +0300

    Python

--- Python_Basic_Operators/BasicOperators.py
+++ Python_Basic_Operators/BasicOperators.py
@@ -1,8 +1,34 @@
-# Python_Basic_Operators
-# a and b are variables
+'''
+Python Operators
+Operators are used to perform operations on variables and values.
+
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+'''
+# PYTHON ARITHMETIC OPERATORS
+'''
+Operator    	Name	
++	Addition    	x + y	
+-	Subtraction	    x - y	
+*	Multiplication	x * y	
+/	Division	    x / y	
+%	Modulus	        x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+
+'''
+# a and b are operands
 a = 10
 b = 20
-# Arithmetic Operator
+'''PYTHON ARITHMETIC OPERATORS'''
 print(a+b) # Addition
 print(a-b) # Subtraction
 print(a/b) # Division
@@ -12,3 +38,15 @@ print(b**a) # Exponent (b=20 to the power a=10)
 a = 9
 b = 2
 print(a//b) # Floor Division
+print("--------")
+''' PYTHON COMPARISON OPERATOR'''
+a = 49
+b = 99
+# returns boolean
+print(a==b) # False
+print(a!=b) # True
+# print(a<>b) # same as the != operator
+print(a>b) # False
+print(a<b) # true
+print(a>=b) # False
+print(a<=b) # True
