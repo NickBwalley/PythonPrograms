06e92a7aa2470ee3c6288d50aee12388f12acade
NickBwalley <nickbwalley@gmail.com>
2020-07-29 20:29:05 +0300

    Python

---
+++ Loops/WhileLoop.py
@@ -0,0 +1,13 @@
+# Using a while loop print numbers between 1 - 10
+i = 1
+while(i <= 10):
+    print(i)
+    i += 1
+print("************")
+
+i=1
+b=9
+number = int(input("Enter the number:"))
+while i<=10:
+    print("%d X %d = %d \n"%(number,i,number*i))
+    i = i+1
