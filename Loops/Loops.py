5ed5116cd2485c495255a724033e704a2077eba6
NickBwalley <nickbwalley@gmail.com>
2020-07-29 19:57:34 +0300

    Pyton

--- Loops/Loops.py
+++ Loops/Loops.py
@@ -2,6 +2,25 @@
 name = "Nick"
 for str in name:
     print(str)
-print("------------------------------------")
+print()
+# program to print the multiplication table of 5
+list = [1,2,3,4,5,6,7,8,9,10]
+n = 5
+for i in list:
+    c = n*i
+    print(c)
+print()
+# Program to sum numbers in a list using a for loop
+list2 = [5,10,15,20,25,30]
+sum = 0
+for j in list2:
+    sum +=j
+    print(sum)
+print()
+# using the range(start,stop,step size)
+number = int(input("Enter a Number: "))
+for x in range(1,11):
+    y = number * x
+    print(number, "*",x, " = ", y)
 
-# 
+print("------------------------------------")
