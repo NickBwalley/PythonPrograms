aa774051e9980124e66b2dec3c9a13b917fa6e40
NickBwalley <nickbwalley@gmail.com>
2020-08-07 12:04:00 +0300

    java

--- classes_objects2/class_two.py
+++ classes_objects2/class_two.py
@@ -1,7 +1,11 @@
 from class_one import student
 
 student_one = student()
+student_two = student()
 
 st_1 = student_one.student_details("Allan Smith", 19)
+st_2 = student_two.student_details("Nick Biiy", 20)
 print(st_1)
 print(student_one) # prints the memory address of the student_one object
+print("--------------")
+print(st_2)
