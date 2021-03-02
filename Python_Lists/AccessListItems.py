e99ef73db784e4fe914725134cd55d1c1de1c9ed
NickBwalley <nickbwalley@gmail.com>
2021-03-02 11:52:11 +0300

    python_lists

---
+++ Python_Lists/AccessListItems.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1]) # access element at index 1
+print(thislist[-1]) # access the  element from reverse order the (last element)
+# access the range of indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist)
+print(thislist[2:5]) # the second index is included the 5th index is excluded
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4]) # prints all the elements from the beginning excluding "kiwi"
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:]) # prints all the elements from the second index
