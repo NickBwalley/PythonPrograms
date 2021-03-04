cc39a38818bc45f2bc2b45ddc505938cc58123c1
NickBwalley <nickbwalley@gmail.com>
2021-03-04 22:29:51 +0300

    pythonLists

---
+++ Python_Lists/CopyAList.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(thislist)
+print(mylist)
+print("------------")
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
---
+++ Python_Lists/JoinAList.py
@@ -0,0 +1,12 @@
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+print("---------------")
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+print("----------------")
