4267e0bd8a69d417e86728272b13e29ec4ed08d7
NickBwalley <nickbwalley@gmail.com>
2021-03-06 14:31:02 +0300

    variables

--- Variables1/main.py
+++ Variables1/main.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+'''
+CASTING IN PYTHON: If you want to specify the data type of a variable, this can be done with casting.
+'''
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+# GET THE TYPE OF VARIABLE
+print(type(x))
+print(type(y))
+print(type(z))
+# NOTE: VARIABLES ARE CASE SENSITIVE
+# String variables can be declared by using either single or double quotes
+'''
+VARIABLES NAMING CONVENTIONS:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+'''
+
+# Camel Case
+myVariableName = "John"
+# Pascal Case
+MyVariableName = "John"
+# Snake Case
+my_variable_name = "John"
+print("----------------------------------------------------------------")
