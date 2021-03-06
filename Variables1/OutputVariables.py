34d9db7fed52aacf6b4cd805c708b3be8a03e497
NickBwalley <nickbwalley@gmail.com>
2021-03-06 15:01:02 +0300

    python_variables

---
+++ Variables1/OutputVariables.py
@@ -0,0 +1,12 @@
+x = "awesome"
+print("Python is " + x)
+y = "Python is "
+z = y+x
+print(z)
+'''
+NOTE: f you try to combine a string and a number, Python will give you an error:
+x = 5
+y = "John"
+print(x + y)
+
+'''
