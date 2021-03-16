2730cffb018ae314478dc005c9baff2b150d10e7
NickBwalley <nickbwalley@gmail.com>
2021-03-16 10:02:06 +0300

    python_strings

---
+++ Strings/python_strings.py
@@ -0,0 +1,18 @@
+
+print("-----------------------------------")
+# \n the escape sequence used to print to a new line
+print("My name is NickBwalley\nI am a Coder")
+# Prints a quotation mark on bronze Bomber word
+print("Also Called the \"Bronze_Bomber\"")
+phrase = "PYCharm"
+print(phrase + " Is a cool IDE")  # basically concatenations
+print(phrase.lower())  # converts the phrase variable to lowerCase
+print(phrase.upper())  # converts the phrase variable to upperCase
+print(phrase.isupper())  # function to check if the phrase variable is uppercase returns false if not
+print(phrase.islower())  # function to chek if the phrase variable is lowerCase returns false if not
+print(phrase.upper().isupper())  # converts the string to upperCase then returns True
+print(phrase.lower().islower())  # converts the string to lowercase then returns True
+print(len(phrase))  # outputs the length of a string defined in your variable that is
+print(phrase[0])  # output the first letter inside our variable phrase >> it will start at 0 index
+print(phrase.index("rm"))  # returns the index of the variable string
+print(phrase.replace("PYCharm", "NickBwalley"))  # function to replace a value with another
