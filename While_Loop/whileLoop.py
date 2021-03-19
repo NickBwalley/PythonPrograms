b90fa88b140831767ea44b16082484516a4b8b7a
NickBwalley <nickbwalley@gmail.com>
2021-03-19 14:31:43 +0300

    python_while_loops

--- While_Loop/whileLoop.py
+++ While_Loop/whileLoop.py
@@ -1,6 +1,46 @@
-#while loop
+"""
+Python Loops
+Python has two primitive loop commands:
+
+while loops
+for loops
+"""
+# The while Loop
+# With the while loop we can execute a set of statements as long as a condition is true.
 i = 1
-while i <= 10:
-    print(i)#Loops 10Times
-    i+=1#increments by one
-print("Done with Loop")
+while i < 6:
+    print(i)
+    i += 1
+print()
+# Note: remember to increment i, or else the loop will continue forever.
+# The break Statement
+# With the break statement we can stop the loop even if the while condition is true:
+# Exit the loop when i is 3:
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+
+# The continue Statement
+# With the continue statement we can stop the current iteration, and continue with the next:
+# Continue to the next iteration if i is 3:
+i = 0
+while(i < 6):
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+# The else Statement
+# With the else statement we can run a block of code once when the condition no longer is true:
+# Print a message once the condition is false:
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+
