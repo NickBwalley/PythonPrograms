0df91adf4b6a02e53df2c04b3a9b2b96db896ed0
NickBwalley <nickbwalley@gmail.com>
2021-03-24 18:28:05 +0300

    python try except

--- TryExcept/try_and_catch.py
+++ TryExcept/try_and_catch.py
@@ -1,10 +1,10 @@
-#Try_Except_Block
+# Try_Except_Block
 try:
     Number = int(input("Enter Your Favorite Number: "))
     print(Number)
     Division = 10/0
     print(Division)
 except ZeroDivisionError as err:
-    print(err)#prints out a specific error (divided by zero )
+    print(err)# prints out a specific error (divided by zero )
 except ValueError as value:
-    print(value)#prints out the compiler error
+    print(value)# prints out the compiler error
