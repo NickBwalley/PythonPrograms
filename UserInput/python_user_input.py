fa2f1337258f8ccfc6d7e27525d0805190e4b613
NickBwalley <nickbwalley@gmail.com>
2021-03-24 18:33:51 +0300

    python get user input

---
+++ UserInput/python_user_input.py
@@ -0,0 +1,22 @@
+"""
+User Input
+Python allows for user input.
+
+That means we are able to ask the user for input.
+
+The method is a bit different in Python 3.6 than Python 2.7.
+
+Python 3.6 uses the input() method.
+
+Python 2.7 uses the raw_input() method.
+"""
+# The following example asks for the username, and when you entered the username, it gets printed on the screen:
+# python 3.6 and higher
+username = input("Enter username:")
+print("Username is: " + username)
+# Python stops executing when it comes to the input() function, and continues when the user has given some input.
+"""
+Python 2.7
+username = raw_input("Enter username:")
+print("Username is: " + username)
+"""
