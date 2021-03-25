005cd5be3ebfecf4e70d350d44f6523f405b87f8
NickBwalley <nickbwalley@gmail.com>
2021-03-25 10:54:26 +0300

    python file handling

--- FileHandling/python_write_create_files.py
+++ FileHandling/python_write_create_files.py
@@ -16,3 +16,37 @@ f.close()
 # open and read the file after appending
 f = open("demofile2.txt", "r")
 print(f.read())
+
+print("--------------------")
+
+# Open the file "demofile3.txt" and overwrite the content:
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+# open and read the file after the appending:
+f = open("demofile3.txt", "r")
+print(f.read())
+# Note: the "w" method will overwrite the entire file.
+
+print("------------------------")
+
+"""
+Create a New File
+To create a new file in Python, use the open() method, with one of the following parameters:
+
+"x" - Create - will create a file, returns an error if the file exist
+
+"a" - Append - will create a file if the specified file does not exist
+
+"w" - Write - will create a file if the specified file does not exist
+"""
+
+# Example
+# Create a file called "myfile.txt":
+f = open("myfile.txt", "x")     # Result: a new empty file is created!
+
+# Example
+# Create a new file if it does not exist:
+# f = open("myfile.txt", "w") # this returns an error because the file already exists
+
