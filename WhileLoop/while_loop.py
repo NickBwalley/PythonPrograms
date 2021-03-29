c582093de92036fa670986f1f1b3ef1ee7dd65dc
NickBwalley <nickbwalley@gmail.com>
2021-03-29 20:32:25 +0300

    python while loop

--- WhileLoop/while_loop.py
+++ WhileLoop/while_loop.py
@@ -28,7 +28,7 @@ while i < 6:
 # With the continue statement we can stop the current iteration, and continue with the next:
 # Continue to the next iteration if i is 3:
 i = 0
-while(i < 6):
+while i < 6:
     i += 1
     if i == 3:
         continue
