12fc60498d2424d92de27d0f8c8f17a675c10bb7
NickBwalley <nickbwalley@gmail.com>
2021-03-31 16:31:14 +0300

    python_operators

--- Operators/python_operators.py
+++ Operators/python_operators.py
@@ -159,16 +159,16 @@ print("--------------------------------------------")
 a = 10
 b = 20
 list = [1, 2, 3, 4, 5 ]
-if  a in list :
+if a in list:
     print ("Line 1 - a is available in the given list")
 else:
     print ("Line 1 - a is not available in the given list")
-if ( b not in list ):
+if b not in list:
     print ("Line 2 - b is not available in the given list")
 else:
     print ("Line 2 - b is available in the given list")
 a = 2
-if ( a in list ):
+if a in list:
     print ("Line 3 - a is available in the given list")
 else:
     print ("Line 3 - a is not available in the given list")
@@ -177,10 +177,10 @@ print("--------------------------------------------")
 ''' PYTHON IDENTITY OPERATOR'''
 a = 20
 b = 20
-if ( a is b ):
-    print ("Line 1 - a and b have same identity")
+if a is b:
+    print("Line 1 - a and b have same identity")
 else:
-    print ("Line 1 - a and b do not have same identity")
+    print("Line 1 - a and b do not have same identity")
 if ( id(a) == id(b) ):
     print ("Line 2 - a and b have same identity")
 else:
