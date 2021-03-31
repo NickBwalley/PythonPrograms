8a244c8d6b82656bc9dfe88e8b63c06e6cc4cce6
NickBwalley <nickbwalley@gmail.com>
2021-03-31 16:09:52 +0300

    python strings

--- Strings/modify_strings.py
+++ Strings/modify_strings.py
@@ -4,6 +4,7 @@ print(a.upper())
 # str to LOWERCASE()
 a = "Hello, World!"
 print(a.lower())
+
 # REMOVE WHITE SPACES
 # Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
 # The strip() method removes any whitespace from the beginning or the end:
