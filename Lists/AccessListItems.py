e08587b9b98a9c66ebfe1effd45c226f534ef5fe
NickBwalley <nickbwalley@gmail.com>
2021-04-01 09:27:59 +0300

    python lists

--- Lists/AccessListItems.py
+++ Lists/AccessListItems.py
@@ -1,11 +1,11 @@
-thislist = ["apple", "banana", "cherry"]
-print(thislist[1]) # access element at index 1
-print(thislist[-1]) # access the  element from reverse order the (last element)
+this_list = ["apple", "banana", "cherry"]
+print(this_list[1])  # access element at index 1
+print(this_list[-1])  # access the  element from reverse order the (last element)
 # access the range of indexes
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist)
-print(thislist[2:5]) # the second index is included the 5th index is excluded
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[:4]) # prints all the elements from the beginning excluding "kiwi"
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:]) # prints all the elements from the second index
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_list)
+print(this_list[2:5])  # the second index is included the 5th index is excluded
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_list[:4])  # prints all the elements from the beginning excluding "kiwi"
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_list[2:])  # prints all the elements from the second index
