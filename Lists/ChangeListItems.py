71408de916f8f3046b8fed27619a637456ddb94b
NickBwalley <nickbwalley@gmail.com>
2021-04-01 09:30:59 +0300

    python lists

--- Lists/ChangeListItems.py
+++ Lists/ChangeListItems.py
@@ -1,19 +1,19 @@
-thislist = ["apple", "banana", "cherry"]
-thislist[1] = "blackcurrant" # replaces the element of the 1st index with "blackcurrant"
-print(thislist)
+this_list = ["apple", "banana", "cherry"]
+this_list[1] = "blackcurrant"    # replaces the element of the 1st index with "blackcurrant"
+print(this_list)
 # change a range of item values
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-thislist[1:3] = ["Lemon", "watermelon"] # replaces the first and second index values
-print(thislist)
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+this_list[1:3] = ["Lemon", "watermelon"]    # replaces the first and second index values
+print(this_list)
 # change the second value by replacing it with two new values
-thislist = ["apple", "banana", "cherry"]
-thislist[1:2] = ["blackcurrant", "watermelon"] # replaces two values in index 1
-print(thislist)
+this_list = ["apple", "banana", "cherry"]
+this_list[1:2] = ["blackcurrant", "watermelon"]  # replaces two values in index 1
+print(this_list)
 # change the second the third value by replacing with one value
-thislist = ["apple", "banana", "cherry"]
-thislist[1:3] = ["watermelon"] # replaces the first index value with "watermelon"
-print(thislist)
+this_list = ["apple", "banana", "cherry"]
+this_list[1:3] = ["watermelon"]     # replaces the first index value with "watermelon"
+print(this_list)
 # insert items
-thislist = ["apple", "banana", "cherry"]
-thislist.insert(2, "watermelon") # insert method inserts and item at a specified index
-print(thislist)
+this_list = ["apple", "banana", "cherry"]
+this_list.insert(2, "watermelon")   # insert method inserts and item at a specified index
+print(this_list)
