ce5ce02442da9ac9eb99a4a753ccaae60829f55e
NickBwalley <nickbwalley@gmail.com>
2021-04-01 09:37:52 +0300

    python lists

--- Lists/loop_through_lists.py
+++ Lists/loop_through_lists.py
@@ -1,22 +1,22 @@
 # print all the items in the list one by one
-thislist = ["apple", "banana", "cherry"]
-for x in thislist:
+this_list = ["apple", "banana", "cherry"]
+for x in this_list:
     print(x)
 print("--------------")
 # loop through the index numbers
-thislist = ["apple", "banana", "cherry"]
-for i in range(len(thislist)):
-    print(thislist[i])
+this_list = ["apple", "banana", "cherry"]
+for i in range(len(this_list)):
+    print(this_list[i])
 print("--------------")
 # using while loop
-thislist = ["apple", "banana", "cherry"]
+this_list = ["apple", "banana", "cherry"]
 i = 0
-while i < len(thislist):
-    print(thislist[i])
-    i+=1
+while i < len(this_list):
+    print(this_list[i])
+    i += 1
 print("--------------")
 # looping using list comprehension
 # list comprehension offers the shortest syntax of looping through lists
 # a short hand for loop that will print all the items in a list
-thislist = ["apples", "banana", "cherry"]
-[print(x) for x in thislist]
+this_list = ["apples", "banana", "cherry"]
+[print(x) for x in this_list]
