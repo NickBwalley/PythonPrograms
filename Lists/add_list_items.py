bb660d254c7062489308526b47bb8b4429e7b43c
NickBwalley <nickbwalley@gmail.com>
2021-04-01 09:34:24 +0300

    python lists

--- Lists/add_list_items.py
+++ Lists/add_list_items.py
@@ -1,20 +1,20 @@
 # append() add an item at the end of the list
-thislist = ["apple", "banana", "cherry"]
-thislist.append("orange")
-print(thislist)
+this_list = ["apple", "banana", "cherry"]
+this_list.append("orange")
+print(this_list)
 # insert() adds an item at a specified index
-thislist = ["apple", "banana", "cherry"]
-thislist.insert(1, "orange")
-print(thislist)
+this_list = ["apple", "banana", "cherry"]
+this_list.insert(1, "orange")
+print(this_list)
 # to append elements from another list to the current list the extend() is useful
-thislist = ["apple", "banana", "cherry"]
+this_list = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
-thislist.extend(tropical)
-print(thislist)
+this_list.extend(tropical)
+print(this_list)
 # add iterables to the list
 # The extend() method does not have to append lists,
 # you can add any iterable object (tuples, sets, dictionaries etc.)
-thislist = ["apple", "banana", "cherry"]
-thistuple = ("kiwi", "orange")
-thislist.extend(thistuple)
-print(thislist)
+this_list = ["apple", "banana", "cherry"]
+this_tuple = ("kiwi", "orange")
+this_list.extend(this_tuple)
+print(this_list)
