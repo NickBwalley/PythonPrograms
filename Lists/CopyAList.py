5546cca398c866267450a7af7ba52258546293e9
NickBwalley <nickbwalley@gmail.com>
2021-04-01 09:52:52 +0300

    python lists

--- Lists/CopyAList.py
+++
@@ -1,8 +0,0 @@
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist.copy()
-print(thislist)
-print(mylist)
-print("------------")
-thislist = ["apple", "banana", "cherry"]
-mylist = list(thislist)
-print(mylist)
---
+++ Lists/copy_lists.py
@@ -0,0 +1,8 @@
+this_list = ["apple", "banana", "cherry"]
+my_list = this_list.copy()
+print(this_list)
+print(my_list)
+print("------------")
+this_list = ["apple", "banana", "cherry"]
+my_list = list(this_list)
+print(my_list)
