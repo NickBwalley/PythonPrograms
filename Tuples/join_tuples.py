f12a2dcebaf07168b84f47fe1936c19a64787c09
NickBwalley <nickbwalley@gmail.com>
2021-04-02 16:17:54 +0300

    python tuples

--- Tuples/join_tuples.py
+++ Tuples/join_tuples.py
@@ -14,6 +14,6 @@ print(tuple3)
 # Multiple Tuples
 # If you want to multiply the content of a tuple a given number of times, you can use the * operator:
 fruits = ("apple", "banana", "cherry")
-mytuple = fruits * 2
+my_tuple = fruits * 2
 
-print(mytuple)
+print(my_tuple)
