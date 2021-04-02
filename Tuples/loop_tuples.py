69a96f71665daa4f08c678123b41687f334d77ec
NickBwalley <nickbwalley@gmail.com>
2021-04-02 16:19:26 +0300

    python tuples

--- Tuples/loop_tuples.py
+++ Tuples/loop_tuples.py
@@ -3,9 +3,9 @@ Loop Through a Tuple
 You can loop through the tuple items by using a for loop.
 """
 # Iterate through the items and print the values:
-thistuple = ("apple", "banana", "cherry")
-for x in thistuple:
-  print(x)
+this_tuple = ("apple", "banana", "cherry")
+for x in this_tuple:
+    print(x)
 
 
 print()
@@ -15,8 +15,8 @@ You can also loop through the tuple items by referring to their index number.
 
 Use the range() and len() functions to create a suitable iterable.
 """
-for x in range(len(thistuple)):
-    print(thistuple[x])
+for x in range(len(this_tuple)):
+    print(this_tuple[x])
 
 """
 Using a While Loop
@@ -28,6 +28,6 @@ Remember to increase the index by 1 after each iteration.
 """
 print()
 i = 0
-while(i < len(thistuple)):
-    print(thistuple[i])
-    i+=1
+while i < len(this_tuple):
+    print(this_tuple[i])
+    i += 1
