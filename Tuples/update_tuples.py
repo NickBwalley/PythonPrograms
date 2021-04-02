e5ea8b58924a1d4a7d6c92f65d490c7ab7b3f3b9
NickBwalley <nickbwalley@gmail.com>
2021-04-02 16:28:00 +0300

    python tuples

--- Tuples/update_tuples.py
+++ Tuples/update_tuples.py
@@ -7,9 +7,11 @@ and convert the list back into a tuple.
 """
 x = ("apple", "banana", "cherry")
 print(x)
-y = list(x)
-y[1] = "kiwi"
-x = tuple(y)
+print(type(x))  # tuple
+
+y = list(x) # convert the tuple to a list
+y[1] = "kiwi"   # changing the value of the index[1] to "kiwi"
+x = tuple(y)  # convert back to a tuple after change has been made
 print(x)
 
 # Once a tuple is created, you cannot add items to it.
@@ -47,3 +49,10 @@ print(this_tuple)
 thistuple = ("apple", "banana", "cherry")
 del thistuple
 # print(thistuple) # this will raise an error because the tuple no longer exists
+
+"""
+Remove() deletes the matching element from the list whereas the del and pop removes the element present at
+ specified index. Difference between pop and del is that pop returns the deleted value whereas del does not.
+When you use the del() when the item is not in the list it throws an error while when you use the remove() 
+even if the item is not present in the list it will not throw an error. 
+"""
