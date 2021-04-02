312599325293229ad63e4d5d841d2c011bd35f3b
NickBwalley <nickbwalley@gmail.com>
2021-04-02 16:01:37 +0300

    python

--- Comments/python_comments.py
+++ Comments/python_comments.py
@@ -1,16 +1,21 @@
-#This is just a commented line
-'''
+# This is just a commented line
+"""
 This is just another comment section todays date is 31-05-2020
 This corona Virus pandemic has shocked the world has its cure has never been found
 This also has caused alot of people to loose their lives as well as drop the entire worlds economy
-'''
-#Two common uses for comments in code
-'''
+
+"""
+# Two common uses for comments in code
+"""
 To write out short notes of a certain line of code
 To comment out a suspected erroneous line of code
-'''
-def print_Crap(name):
+"""
+
+
+def print_crap(name):
     return name
+
+
 name = (input("What is Your Name: "))
-print_Crap(name)
+print_crap(name)
 print("Your name is: "+name)
