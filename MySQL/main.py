4e3d8e6c261f3bb4a9ecdbcea7bb4a72ada3ebcb
NickBwalley <nickbwalley@gmail.com>
2021-04-03 15:33:36 +0300

    MYSQL

---
+++ MySQL/main.py
@@ -0,0 +1,6 @@
+# install MySQL server
+# ubuntu terminal
+# sudo apt update (make sure all the packages are up to date)
+# sudo apt install mysql-server
+# mysql --version (confirm that mysql has been installed)
+
