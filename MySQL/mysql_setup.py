1b62341be71d66b460716a1fb7ba781c42de2372
NickBwalley <nickbwalley@gmail.com>
2021-04-03 15:45:53 +0300

    MYSQL

--- MySQL/mysql_setup.py
+++ MySQL/mysql_setup.py
@@ -13,5 +13,28 @@
 # mysql-> quit # bye
 
 
+"""
+Install MySQL Driver
+Python needs a MySQL driver to access the MySQL database.
+
+In this tutorial we will use the driver "MySQL Connector".
+
+We recommend that you use PIP to install "MySQL Connector".
+
+PIP is most likely already installed in your Python environment.
+
+Navigate your command line to the location of PIP, and type the following:
+
+Download and install "MySQL Connector":
+
+windows:
+    C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>python -m pip install
+    mysql-connector-python
+linux:
+    $ sudo apt update
+    $ python3 -m pip install mysql-connector-python
+"""
+
+
 
 
