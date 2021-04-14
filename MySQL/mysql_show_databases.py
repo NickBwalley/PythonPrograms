930a979ba1558e4f1ed8d4c3a10d87a00b2693a4
NickBwalley <nickbwalley@gmail.com>
2021-04-14 11:23:07 +0300

    python MYSQL

---
+++ MySQL/mysql_show_databases.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+my_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="N!ckb!!y.mysql099"
+)
+
+my_cursor = my_db.cursor()
+
+my_cursor.execute("SHOW DATABASES")
+
+for x in my_cursor:
+    print(x)
